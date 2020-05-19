19e271f72c74d028739288a22f89a6fcf02e7e0d
Emerson Yurivilca <emersonyuri32@gmail.com>
2020-05-19 12:59:08 -0500

    Bad changes on new path division

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,27 +1,16 @@
 """platzigram URL Configuration"""
+
+# Django
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-
-from platzigram import views as local_views
-from posts import views as posts_views
-from users import views as users_views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('hello_world/', local_views.hello_world, name='hello_world'),
-    path('sorted/', local_views.sort_integers, name='sort'),
-    path('hi/<str:name>/<int:age>/', local_views.say_hi, name='hi'),
+    path('', include(('posts.urls', 'posts'), namespace='posts')),
 
-    path('', posts_views.list_posts, name='feed'),
-    path('posts/new/', posts_views.create_post, name='create_post'),
-
-    path('users/login/', users_views.login_view, name='login'),
-    path('users/logout/', users_views.logout_view, name='logout'),
-    path('users/signup/', users_views.signup, name='signup'),
-    path('users/me/profile/', users_views.update_profile, name='update_profile'),
-
+    path('users/', include(('users.urls', 'users'), namespace='users')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
