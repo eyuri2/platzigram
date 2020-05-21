84568035b4bba6b64bb23219607efc89daf1f37c
Emerson YR <eyurivilcar@gmail.com>
2020-05-21 00:01:52 -0500

    add final class views for login, register and signup

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,9 @@
 """ Posts views """
 
 # Django
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
-from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 #  Forms
 from posts.forms import PostForm
@@ -16,16 +14,8 @@ from posts.models import Post
 class PostDetailView(LoginRequiredMixin, DetailView):
     """Post detail view."""
     template_name = 'posts/detail.html'
-    slug_field = 'pk'
-    slug_url_kwarg = 'pk'
     queryset = Post.objects.all()
     context_object_name = 'post'
-
-    def get_context_data(self, **kwargs):
-        """Add posts to context"""
-        context = super().get_context_data(**kwargs)
-        context['user'] = self.request.user
-        return context
 
 
 class PostsFeedView(LoginRequiredMixin, ListView):
@@ -33,27 +23,20 @@ class PostsFeedView(LoginRequiredMixin, ListView):
     template_name = 'posts/feed.html'
     model = Post
     ordering = ('-created',)
-    paginate_by = 2
+    paginate_by = 10
     context_object_name = 'posts'
 
 
-@login_required
-def create_post(request):
-    """Create new post view."""
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('posts:feed')
-    else:
-        form = PostForm()
-    
-    return render(
-        request=request,
-        template_name='posts/new.html',
-        context={
-            'form': form,
-            'user': request.user,
-            'profile': request.user.profile
-        }
-    )
+class CreatePostView(LoginRequiredMixin, CreateView):
+    """Create a new post."""
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
+
+    def get_context_data(self, **kwargs):
+        """Add user and profile o context."""
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['profile'] = self.request.user.profile
+        return context
+
