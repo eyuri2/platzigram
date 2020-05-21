8b938b7ee06498ee888db7d0f335a5e0bb52454e
Emerson Yurivilca <emersonyuri32@gmail.com>
2020-05-21 11:43:17 -0500

    Add postgresql database

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'platzigram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'platzigram',
+        'USER': 'miki',
+        'PASSWORD': 'miki3',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
